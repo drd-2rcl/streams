7b338a7d55e18b5c7e1d1ff4e13e5a978554c756
drd-2rcl <drd.rclrd@gmail.com>
2019-05-05 17:52:02 -0300

    refactoring for routes file

--- src/App.js
+++ src/App.js
@@ -1,26 +1,7 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom'
 
-import StreamCreate from './pages/StreamCreate'
-import StreamEdit from './pages/StreamEdit'
-import StreamDelete from './pages/StreamDelete'
-import StreamList from './pages/StreamList'
-import StreamShow from './pages/StreamShow'
+import Routes from './routes';
 
-import Header from './components/Header'
-const App = () => {
-  return (
-    <div>
-      <BrowserRouter>
-        <Header />
-        <Route path="/" exact component={StreamList} />
-        <Route path="/streams/new" exact component={StreamCreate} />
-        <Route path="/streams/edit" exact component={StreamEdit} />
-        <Route path="/streams/delete" exact component={StreamDelete} />
-        <Route path="/streams/show" exact component={StreamShow} />
-      </BrowserRouter>
-    </div>
-  )
-}
+const App = () => <Routes />
 
 export default App;
