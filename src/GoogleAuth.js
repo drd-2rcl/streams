3029327071b93bd31d77e261d8a258d6204dd333
drd-2rcl <drd.rclrd@gmail.com>
2019-05-05 18:13:36 -0300

    logic for apply and rendering authentication stats

--- src/GoogleAuth.js
+++ src/GoogleAuth.js
@@ -4,20 +4,33 @@ import config from './env'
 const { client_id } = config
 
 class GoogleAuth extends Component {
+  state = { isSignedIn: null }
+
   componentDidMount() {
     window.gapi.load('client:auth2', () => {
       window.gapi.client.init({
         client_id,
         scope: 'email'
-      })
-    })
+      }).then(() => {
+        this.auth = window.gapi.auth2.getAuthInstance();
+        this.setState({ isSignedIn: this.auth.isSignedIn.get() })
+      });
+    });
+  }
+
+  renderAuthButton() {
+    if(this.state.isSignedIn === null) {
+      return <div>I dont know if we are signed in</div>;
+    } else if (this.state.isSignedIn) {
+      return <div>I am signed in!</div>
+    } else {
+      return <div>I am not signed in.</div>
+    }
   }
 
   render() {
     return (
-      <div>
-        Google Auth
-      </div>
+      <div>{this.renderAuthButton()}</div>
     )
   }
 }
