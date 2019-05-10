dc70a8df0fbeeae711de0c39ded4111a196e66a0
drd-2rcl <drd.rclrd@gmail.com>
2019-05-09 21:59:46 -0300

    style and handling form submission

--- src/pages/StreamCreate.js
+++ src/pages/StreamCreate.js
@@ -3,11 +3,11 @@ import { Field, reduxForm } from 'redux-form';
 import styled from 'styled-components';
 
 const Container = styled.div`
-justify-content: center;
-align-items: center;
-display: flex;
-height: 50vh;
-color: #000;
+  justify-content: center;
+  align-items: center;
+  display: flex;
+  height: 50vh;
+  color: #000;
 `
 
 const Input = styled.input`
@@ -25,17 +25,51 @@ const Form = styled.form`
   flex-direction: column;
 `;
 
+const Label = styled.label`
+  color: #000;
+  font-size: 1rem;
+  padding: 10px 0;
+  font-weight: bold;
+`;
+
+const Button = styled.button`
+  background: #000;
+  color: #fff;
+  border: 1px solid #DDD;
+  border-radius: 4px;
+  font-size: 1rem;
+  padding: 10px;
+  font-weight: bold;
+  border: 0;
+  cursor: pointer;
+`;
+
 class StreamCreate extends Component {
-  renderInput({ input }) {
-    return <Input { ...input } />
+  renderInput({ input, label }) {
+    return (
+      <div>
+        <div>
+          <Label>{label}</Label>
+        </div>
+        <div>
+          <Input { ...input } />
+        </div>
+      </div>
+    )
+  }
+
+  onSubmit(formValues) {
+    console.log(formValues);
   }
 
   render() {
+    console.log(this.props)
     return (
       <Container>
-        <Form>
-          <Field name="title" component={this.renderInput} />
-          <Field name="description" component={this.renderInput} />
+        <Form onSubmit={this.props.handleSubmit(this.onSubmit)} >
+          <Field name="title" component={this.renderInput} label="Enter Title" />
+          <Field name="description" component={this.renderInput} label="Enter Description" />
+          <Button>Submit</Button>
         </Form>
       </Container>
     )
