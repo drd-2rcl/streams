03f0295f0e1390a48943afed84be1c3b127a63eb
drd-2rcl <drd.rclrd@gmail.com>
2019-05-09 21:39:53 -0300

    style and build automatically handling events

--- src/pages/StreamCreate.js
+++ src/pages/StreamCreate.js
@@ -1,14 +1,43 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import styled from 'styled-components';
+
+const Container = styled.div`
+justify-content: center;
+align-items: center;
+display: flex;
+height: 50vh;
+color: #000;
+`
+
+const Input = styled.input`
+  height: 48px;
+  border: 1px solid #DDD;
+  border-radius: 4px;
+  font-size: 1rem;
+  padding: 0 20px;
+  margin: 10px 0;
+`;
+
+const Form = styled.form`
+  width: auto;
+  display: flex;
+  flex-direction: column;
+`;
 
 class StreamCreate extends Component {
+  renderInput({ input }) {
+    return <Input { ...input } />
+  }
+
   render() {
-    // console.log(this.props);
     return (
-      <form>
-        <Field name="title" />
-        <Field name="description" />
-      </form>
+      <Container>
+        <Form>
+          <Field name="title" component={this.renderInput} />
+          <Field name="description" component={this.renderInput} />
+        </Form>
+      </Container>
     )
   }
 }
