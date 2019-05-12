30ff9f72914205f7de92336ae01227b71a3f205b
drd-2rcl <drd.rclrd@gmail.com>
2019-05-12 11:07:45 -0300

    create new stream with redux-thunk and REST conventions

--- src/pages/StreamCreate.js
+++ src/pages/StreamCreate.js
@@ -84,8 +84,8 @@ class StreamCreate extends Component {
     )
   }
 
-  onSubmit(formValues) {
-    console.log(formValues);
+  onSubmit = formValues => {
+    this.props.createStream(formValues);
   }
 
   render() {
@@ -115,7 +115,9 @@ const validate = (formValues) => {
   return errors
 }
 
-export default reduxForm({
+const formWrapped = reduxForm({
   form: 'streamCreate',
   validate
 })(StreamCreate);
+
+export default connect(null, { createStream })(formWrapped);
