13894fa2a2c5a95019ccb3cfc56f1fd9366b73d3
drd-2rcl <drd.rclrd@gmail.com>
2019-05-12 13:30:10 -0300

    rendering all streams with connect react-redux, componentDidMount for call props fetchStreams and mapStateToProps for receive data state

--- src/pages/StreamList.js
+++ src/pages/StreamList.js
@@ -1,17 +1,43 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { fetchStreams } from '../actions/'
+// import streams from '../apis/streams';
 
 class StreamList extends Component {
   componentDidMount() {
     this.props.fetchStreams();
   }
 
+  renderList() {
+    return this.props.streams.map(stream => {
+      return(
+        <div key={stream.id}>
+          <img alt=""/>
+          <div>
+          {stream.title}
+          <div>{stream.description}</div>
+          </div>
+        </div>
+      )
+    })
+  }
+
   render () {
+    // console.log(this.props.streams)
     return (
-      <div>StreamList</div>
+      <div>
+        <h2>Streams</h2>
+        <div>{this.renderList()}</div>
+      </div>
     )
   }
 } 
 
-export default connect(null, { fetchStreams })(StreamList);
+const mapStateToProps = state => {
+  return { streams: Object.values(state.streams) }
+} 
+
+export default connect(
+  mapStateToProps, 
+  { fetchStreams }
+)(StreamList);
