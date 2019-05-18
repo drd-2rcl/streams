77a303c5e79896332fdf4ddbb4c0d0d0c978089c
drd-2rcl <drd.rclrd@gmail.com>
2019-05-18 14:08:09 -0300

    was created a component modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 
 const Modal = props => {
   return ReactDOM.createPortal(
-    <div>
-      <h1>Hi \o/</h1>
+    <div className="ui dimmer modals visible active">
+      <div className="ui standard modal visible active">
+        <h1>Hi \o/</h1>
+      </div>
     </div>,
     document.querySelector('#modal')
   )
 }
 
-export default Modal; 
+export default Modal;
