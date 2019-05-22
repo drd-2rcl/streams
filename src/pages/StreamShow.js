e811bb472b06cc2cf6f41a8d0cf76ad2fb89bd37
drd-2rcl <drd.rclrd@gmail.com>
2019-05-21 22:58:22 -0300

    optional player building

--- src/pages/StreamShow.js
+++ src/pages/StreamShow.js
@@ -1,11 +1,37 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux';
-import { fetchStream } from '../actions';
 import flv from 'flv.js';
 
+import { fetchStream } from '../actions';
+
 class StreamShow extends Component {
+  constructor(props) {
+    super(props);
+    this.videoRef = React.createRef();
+  }
+
   componentDidMount() {
-    this.props.fetchStream(this.props.match.params.id)
+    const { id } = this.props.match.params;
+    this.props.fetchStream(id);
+    this.buildPlayer();
+  }
+
+  componentDidUpdate() {
+    this.buildPlayer();
+  }
+
+  buildPlayer() {
+    if ( this.player || !this.props.stream ) {
+      return;
+    }
+
+    const { id } = this.props.match.params;
+    this.player = flv.createPlayer({
+      type: 'flv',
+      url: `ttp://localhost:8000/live${id}.flv`
+    });
+    this.player.attachMediaElement(this.videoRef.current);
+    this.player.load();
   }
 
   render() {
